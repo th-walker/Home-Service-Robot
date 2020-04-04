dc4bf6448420dac0dcff5ca35f4e2d512e0993d3
th-walker <tamzinwalker@gmail.com>
2020-04-04 11:18:52 +0800

    script file edits

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/tamzinwalker.world" &
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/taz/Documents/repos/Home-Service-Robot/catkin_ws/src/map/tamzinwalker.world
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-export ROBOT_INITIAL_POSE="-x 10 -y 10 -Y 0"
+#export ROBOT_INITIAL_POSE="-x 10 -y 10 -Y 0"
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/tamzinwalker.world" &
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/taz/Documents/repos/Home-Service-Robot/catkin_ws/src/map/tamzinwalker.world
+
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
