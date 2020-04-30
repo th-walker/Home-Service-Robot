22af328817b7856744f49d28fb465cf09c92a097
Tamzin Walker <7385776+th-walker@users.noreply.github.com>
2020-04-30 20:31:56 +0800

    Update add_markers.cpp

--- catkin_ws/src/add_makers/src/add_markers.cpp
+++ catkin_ws/src/add_makers/src/add_markers.cpp
@@ -1,26 +1,36 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include "nav_msgs/Odometry.h"
 
-int main( int argc, char** argv )
-{
-  ros::init(argc, argv, "basic_shapes");
+
+class Marker{
+  private:
+    double pickUp[2] = {-1.6, 3.3};
+    double dropOff[2] = {-2.5, -5.6};
+    int goal_state = 1;
+    const int moving = 1;
+    const int pickingUp = 2;
+    const int droppingOff = 3;
+  
+public:
+   void displayMarker(double x, double y, int action)
+  {
+  
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-
-  // Set our initial shape type to be a cube
+  ros::Subscriber odom_sub = n.subscribe("/odom", 1000, odomCallback);
+     // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
-
-  while (ros::ok())
-  {
-    visualization_msgs::Marker marker;
+  
+  visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
     marker.header.stamp = ros::Time::now();
 
     // Set the namespace and id for this marker.  This serves to create a unique ID
     // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "basic_shapes";
+    marker.ns = "add_markers";
     marker.id = 0;
 
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
@@ -29,19 +39,10 @@ int main( int argc, char** argv )
     // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     marker.action = visualization_msgs::Marker::ADD;
 
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = 0.5;
+    marker.scale.y = 0.5;
+    marker.scale.z = 0.5;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
@@ -49,20 +50,74 @@ int main( int argc, char** argv )
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
 
+    marker.pose.position.x = x;
+    marker.pose.position.y = y;
+       
+    if (action == 1)
+     {
+       marker.action = visualization_msgs::Marker::DELETE;
+     }
+    if (action == 2)
+     {
+       marker.action = visualization_msgs::Marker::ADD;
+     }
+  
     marker.lifetime = ros::Duration();
+    
+  publishMarker();
+}
 
-    // Publish the marker
+void publishMarker(){
+ // Publish the marker
     while (marker_pub.getNumSubscribers() < 1)
     {
-      if (!ros::ok())
-      {
-        return 0;
-      }
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-    marker_pub.publish(marker);
+    marker_pub.publish(marker);  
+}
 
-    r.sleep();
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  float current_pose_x = msg->pose.pose.position.x;
+  float current_pose_y = msg->pose.pose.position.y;
+  
+  if ((pickUp[0] - current_pose_x) + (pickUp[1] - current_pose_y) < 1){
+   goal_state = pickingUp; 
+  }
+  else if ((dropOff[0] - current_pose_x) + (dropOff[1] - current_pose_y) < 1){
+   goal_state = droppingOff; 
+  }
+  
+  switch(goal_state){
+    case moving:
+      ROS_INFO("Position-> x: [%f], y: [%f]", current_pose_x, current_pose_x);
+      break;
+    case pickingUp:
+      sleep(5);
+      displayMarker(pickUp[0], pickUp[1], 2);
+      break;
+    case droppingOff:
+      sleep(5);
+      displayMarker(dropOff[0], dropOff[1], 2);
+      break;
+    default:
+      //ROS_INFO("Position-> x: [%f], y: [%f]", current_pose_x, current_pose_x);
+      break;
+  } 
+}
+};
+
+int main( int argc, char** argv )
+{
+  
+  ros::init(argc, argv, "add_markers");
+  marker.ns = "add_markers";
+  
+  
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    
   }
 }
